b6243b24a9c6cf7d51c440a8a7334bf921394407
www-data <www-data@f739ec2587b7>
2019-02-07 15:34:47 +0000

    [VP] Updated 2 options
    VP-Action: option/update/newsletter_diagnostic_cron_calls
    
    VP-Action: option/update/newsletter_diagnostic_cron_data
    
    X-VP-Version: 4.0-beta
    X-VP-Environment: default

--- wp-content/plugins/strong-testimonials/admin/partials/views/template-input.php
+++
@@ -1,8 +0,0 @@
-<div>
-    <input type="radio" id="template-<?php esc_attr_e( $key ); ?>"
-           name="view[data][<?php esc_attr_e( $current_mode ); ?>]"
-           value="<?php esc_attr_e( $key ); ?>" <?php checked( $key, $view['template'] ); ?>>
-    <label for="template-<?php esc_attr_e( $key ); ?>">
-		<?php echo $template['config']['name']; ?>
-    </label>
-</div>

5bf28177d2adf7cd6f26e4713284d2e218236a70
root <s.kammoun@enano-tech.com>
2019-02-13 11:35:22 +0000

    [VP] Theme switched to 'ODF theme 1'
    VP-Action: theme/switch/ODF-theme-1
    VP-Theme-Name: ODF theme 1
    
    X-VP-Version: 4.0-beta
    X-VP-Environment: default

---
+++ wp-content/themes/ODF-theme-1/template-parts/custom-single_product_sheet.php
@@ -0,0 +1,57 @@
+
+
+    <div class="product-detail-2">
+      <div class="img-medium-product">
+        <img src="/wp-content/themes/ODF-theme-2/template-parts/img/product-2.png" />
+      </div>
+      <div class="detail-product">
+        <br>
+        <h1>Product sheet 1</h1>
+        <p class="product-description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+        <h4>Caracteristics</h4>
+        <p class="product-description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <div class="details-product">
+            <div class="detail-product-icon">
+              <img src="/wp-content/themes/ODF-theme-2/template-parts/img/detail-product-1.png" />
+              <span>Icon Name</span>
+            </div>
+            <div class="detail-product-icon">
+              <img src="/wp-content/themes/ODF-theme-2/template-parts/img/detail-product-2.png" />
+              <span>Icon Name</span>
+            </div>
+            <div class="detail-product-icon">
+              <img src="/wp-content/themes/ODF-theme-2/template-parts/img/detail-product-3.png" />
+              <span>Icon Name</span>
+            </div>
+            <div class="detail-product-icon">
+              <img src="/wp-content/themes/ODF-theme-2/template-parts/img/detail-product-4.png" />
+              <span>Icon Name</span>
+            </div>
+        </div>
+      </div>
+      <div class="product-detail-select">
+        <form>
+          <select class="form-control" class="selectbox">
+            <option>SELECT</option>
+            <option>FAQ</option>
+            <option>TESTIMONIALS</option>
+            <option>ADVICES</option>
+          </select>
+        </form>
+      </div>
+    </div>
+    <div class="clear"></div>
+    <div class="btns-single-product">
+      <a href="#" class="request-sample"><span class="icon-btn"><img src="/wp-content/themes/ODF-theme-2/template-parts/img/btn1-detail-product.png" /></span>Request sample</a>
+      <a href="#" class="buy"><span class="icon-btn"><img src="/wp-content/themes/ODF-theme-2/template-parts/img/btn3-detail-product.png" /></span>Buy</a>
+      <a href="#" class="find-shop"><span class="icon-btn"><img src="/wp-content/themes/ODF-theme-2/template-parts/img/btn2-detail-product.png" /></span>Find shop</a>
+    </div>
+    <h2 class="demo-product">DEMO PRODUCT</h2>
+    <div class="demo-product-2">
+      <img src="/wp-content/themes/ODF-theme-2/template-parts/img/demo-product-2.png" />
+    </div>
+

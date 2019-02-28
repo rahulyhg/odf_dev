4ecde4b1ddc34215389a26546ff023df9a4026ae
root <s.kammoun@enano-tech.com>
2019-02-28 13:15:40 +0000

    [VP] Deactivated plugin 'Homepage Settings'
    VP-Action: plugin/deactivate/add-wordpress-settings-page/add-wordpress-settings-page.php
    VP-Plugin-Name: Homepage Settings
    
    X-VP-Version: 4.0-beta
    X-VP-Environment: default

--- wp-content/plugins/add-wordpress-settings-page/add-wordpress-settings-page.php
+++ wp-content/plugins/add-wordpress-settings-page/add-wordpress-settings-page.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Homepage Settings for BigBang
+Plugin Name: Homepage Settings
 Plugin URI: http://www.inboundnow.com/
 Description: Adds additional functionality to the big bang theme.
 Author: David Wells
@@ -29,19 +29,77 @@ function sampleoptions_init_fn(){
 	add_settings_field('radio_buttons', 'Select Shape', 'setting_radio_fn', __FILE__, 'main_section');
 	add_settings_field('radio_buttons', 'Home Page Boxes', 'setting_visual_fn', __FILE__, 'main_section');
 	add_settings_field('drop_down1', 'Select Color', 'setting_dropdown_fn', __FILE__, 'main_section');
-	add_settings_field('plugin_chk1', 'Restore Defaults Upon Reactivation?', 'setting_chk1_fn', __FILE__, 'main_section');
+	add_settings_field(
+		'plugin_chk1', 
+		'Restore Defaults Upon Reactivation?', 
+		'setting_chk1_fn', 
+		__FILE__, 
+		'main_section'
+	);
+
+	/*add_settings_field( // Option 1
+        'header_background', // Option ID
+        'Logo URL', // Label
+        'logo_fn', // !important - This is where the args go!
+        __FILE__, // Page it will be displayed (General Settings)
+        'main_section', // Name of our section
+        array( // The $args
+            'header_background' // Should match Option ID
+        )
+    );
+	add_settings_field( // Option 1
+        'favicon_url', // Option ID
+        'Favicon URL', // Label
+        'favicon_fn', // !important - This is where the args go!
+        __FILE__, // Page it will be displayed (General Settings)
+        'main_section', // Name of our section
+        array( // The $args
+            'favicon_url' // Should match Option ID
+        )
+    );
+
+    register_setting('general', 'logo_url', 'esc_attr');
+    register_setting('general', 'favicon_url', 'esc_attr');*/
 }
 // Add sub page to the Settings Menu
 function sampleoptions_add_page_fn() {
-// add optiont to main settings panel
- add_options_page('Big Bang Extra Settings', 'BigBang Settings', 'administrator', __FILE__, 'options_page_fn');
+	// add optiont to main settings panel
+	add_options_page('Extra Settings', 'Theme options', 'administrator', __FILE__, 'options_page_fn');
 }
 // ************************************************************************************************************
 // Callback functions
 // Init plugin options to white list our options
 // Section HTML, displayed before the first option
+
+
+
+
+
+function logo_fn($args)
+{
+    $option = get_option($args[0]);
+    echo '<input type="text" class="settings_input_media_url" id="' . $args[0] . '" name="' . $args[0] . '" value="' . $option . '" />';
+}
+
+function favicon_fn($args)
+{
+    $option = get_option($args[0]);
+    echo '<input type="text" class="settings_input_media_url" id="' . $args[0] . '" name="' . $args[0] . '" value="' . $option . '" />';
+}
+
+
+
+
+
+
+
+
+
+
+
+
 function  section_text_fn() {
-	echo '<p>Below are some examples of different option controls.</p>';
+	echo '<p>Below are some fields of different option controls.</p>';
 }
 // DROP-DOWN-BOX - Name: plugin_options[dropdown1]
 function  setting_dropdown_fn() {
@@ -96,11 +154,11 @@ function setting_visual_fn() {
 	$args = array("textarea_name" => "plugin_options[textarea_one]");
 	wp_editor( $options['textarea_one'], "plugin_options[textarea_one]", $args );
 	
-// Add another text box
+	// Add another text box
 	$options = get_option('plugin_options');
 	$args = array("textarea_name" => "plugin_options[textarea_two]");
 	wp_editor( $options['textarea_two'], "plugin_options[textarea_two]", $args );				
-	}		
+}		
 // Sanitize and validate input. Accepts an array, return a sanitized array.
 function wpet_validate_options($input) {
 	// Sanitize textarea input (strip html tags, and escape characters)
@@ -111,21 +169,64 @@ function wpet_validate_options($input) {
 }
 // Display the admin options page
 function options_page_fn() {
+	wp_enqueue_media();
 ?>
+	<script>
+		/* background configuration via amdin*/
+	        jQuery('#header_background').click(function(e) {
+	            e.preventDefault();
+
+	            var custom_uploader = wp.media({
+	                title: 'Custom Image',
+	                button: {
+	                    text: 'Upload Image'
+	                },
+	                multiple: false  // Set this to true to allow multiple files to be selected
+	            })
+	            .on('select', function() {
+	                var attachment = custom_uploader.state().get('selection').first().toJSON();
+	                jQuery('#header_background').attr('value', attachment.url);
+	                // $j('.header_logo_url').val(attachment.url);
+
+	            })
+	            .open();
+	        });
+	        jQuery('#favicon_url').click(function(e) {
+	            e.preventDefault();
+
+	            var custom_uploader_favicon = wp.media({
+	                title: 'Custom Image',
+	                button: {
+	                    text: 'Upload Image'
+	                },
+	                multiple: false  // Set this to true to allow multiple files to be selected
+	            })
+	            .on('select', function() {
+	                var attachment = custom_uploader_favicon.state().get('selection').first().toJSON();
+	                jQuery('#favicon_url').attr('value', attachment.url);
+	            })
+	            .open();
+	        });
+	    /* background configuration via amdin*/
+	</script>
+	<style type="text/css" media="screen">
+		.settings_input_media_url {
+		    width: 50%;
+		}
+	</style>
 	<div class="wrap">
 		<div class="icon32" id="icon-options-general"><br></div>
-		<h2>My Example Options Page</h2>
-		Some optional text here explaining the overall purpose of the options and what they relate to etc.
+		<h2>Theme options</h2>
 		<form action="options.php" method="post">
-					<?php
-if ( function_exists('wp_nonce_field') ) 
-	wp_nonce_field('plugin-name-action_' . "yep"); 
-?>
-		<?php settings_fields('plugin_options'); ?>
-		<?php do_settings_sections(__FILE__); ?>
-		<p class="submit">
-			<input name="Submit" type="submit" class="button-primary" value="<?php esc_attr_e('Save Changes'); ?>" />
-		</p>
+			<?php
+			if ( function_exists('wp_nonce_field') ) 
+				wp_nonce_field('plugin-name-action_' . "yep"); 
+			?>
+			<?php settings_fields('plugin_options'); ?>
+			<?php do_settings_sections(__FILE__); ?>
+			<p class="submit">
+				<input name="Submit" type="submit" class="button-primary" value="<?php esc_attr_e('Save Changes'); ?>" />
+			</p>
 		</form>
 	</div>
 <?php
@@ -136,3 +237,8 @@ function plugin_options_validate($input) {
 	$input['text_string'] =  wp_filter_nohtml_kses($input['text_string']);	
 	return $input; // return validated input
 }
+
+
+
+
+

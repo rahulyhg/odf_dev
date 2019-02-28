ed45437d14a58d8c7b1939e9fa5b1dd7726e1eaf
root <s.kammoun@enano-tech.com>
2019-02-28 19:05:09 +0000

    updates catalog page changes

--- wp-content/themes/ODF-theme-1/woocommerce/content-product.php
+++ wp-content/themes/ODF-theme-1/woocommerce/content-product.php
@@ -47,6 +47,7 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 	 * @hooked woocommerce_template_loop_product_title - 10
 	 */
 	do_action( 'woocommerce_shop_loop_item_title' );
+	
 
 	/**
 	 * Hook: woocommerce_after_shop_loop_item_title.
@@ -55,7 +56,8 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 	 * @hooked woocommerce_template_loop_price - 10
 	 */
 	do_action( 'woocommerce_after_shop_loop_item_title' );
-	do_action('woocommerce_archive_description');
+
+	echo get_the_excerpt($product->get_id());
 
 	/**
 	 * Hook: woocommerce_after_shop_loop_item.
